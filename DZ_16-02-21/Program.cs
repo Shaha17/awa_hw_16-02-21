5f50c8f95f13baacdbc303612474e8e4acba8854
ShahaKB <shahzodjabbarov@gmail.com>
2021-02-17 10:46:08 +0500

    initial commit

---
+++ DZ_16-02-21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_16_02_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
